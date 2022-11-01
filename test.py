1172d6154c42b7f598d43da5161a893c3204d0e1
ThiagoHN <thiagohnogueira01@gmail.com>
2022-10-31 22:54:24 -0300

    [feat] Caption parser implemented

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class SRTTest(unittest.TestCase):
     def test_add_caption_at_the_end(self):
         srt_file = SRTParser("src/srt_examples/test.srt")
 
-        srt_file.add_caption_at_the_end("hello", "00:19:49,300")
+        srt_file.add_caption_at_the_end("hello", timedelta(seconds=1))
 
         self.assertTrue(str(srt_file).endswith("hello"), str(srt_file))
 
